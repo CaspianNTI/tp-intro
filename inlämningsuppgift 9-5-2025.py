09cff9e750d272e6f7a808e6287dd0b278445c53
CaspianNTI <caspian.svedjehed@ga.ntig.se>
2025-09-05 14:20:00 +0200

    fgegr

--- inlämningsuppgift 9-5-2025.py
+++ inlämningsuppgift 9-5-2025.py
@@ -12,3 +12,11 @@ while True:  # [att upprepa]
         break
     summa = float(tal1) + float(tal2)  # [att spara]
     print("Summan är:", summa)
+
+#Copilots beskrivning:
+
+#Programmet använder två av dina fraser: [att spara] och [att upprepa].
+
+#[att spara] används för att ta emot två tal från användaren och spara dem i variablerna tal1 och tal2. Summan av dessa tal sparas också i variabeln summa.
+#[att upprepa] används genom en while True-loop, vilket gör att programmet fortsätter att fråga efter nya tal tills användaren skriver "stopp".
+#Varje gång två tal matas in, räknar programmet ut och skriver ut deras summa. Om användaren skriver "stopp" istället för ett tal, avslutas programmet.
