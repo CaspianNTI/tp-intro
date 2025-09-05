31427279fba3406f77bed9e796db9abf9f0d03e0
CaspianNTI <caspian.svedjehed@ga.ntig.se>
2025-09-05 14:25:20 +0200

    nnui

--- inlämningsuppgift 9-5-2025.py
+++ inlämningsuppgift 9-5-2025.py
@@ -15,8 +15,9 @@ while True:  # [att upprepa]
 
 #Copilots beskrivning:
 
-#Programmet använder två av dina fraser: [att spara] och [att upprepa].
+#Programmet använder alla tre fraserna: [att spara], [att upprepa] och [att välja].
 
 #[att spara] används för att ta emot två tal från användaren och spara dem i variablerna tal1 och tal2. Summan av dessa tal sparas också i variabeln summa.
 #[att upprepa] används genom en while True-loop, vilket gör att programmet fortsätter att fråga efter nya tal tills användaren skriver "stopp".
+#[att välja] används med hjälp av två if-satser. Programmet väljer att antingen fortsätta eller avsluta beroende på om användaren skriver "stopp".
 #Varje gång två tal matas in, räknar programmet ut och skriver ut deras summa. Om användaren skriver "stopp" istället för ett tal, avslutas programmet.
